0e805671a49e396424eaeb42ce9e7dfb30980243
Tu Hoang <hatu87@gmail.com>
2017-04-04 07:57:09 +0700

    finish create basic server with hello

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
-  console.log('server is listening in port 3000');
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+var port = 3001;
+app.listen(port, function(){
+  console.log(`server is listening in port ${port}`);
 });
